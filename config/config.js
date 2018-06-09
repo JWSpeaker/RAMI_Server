327fddc67b30070be77f9e22ce40d7b2e890ffe7
RyuJaeChan <rjch221@gmail.com>
2018-06-09 21:52:03 +0900

    add leaning path
    if get /leaning path, then get package.json

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     server_port :3000,
     route_info : [
         {path : "/index", file : "./index", type : "get", method : "index_get"},
-
+        {path : "/leaning", file : "./leaning", type : "get", method : "leaning"},
     ]
 
 
--- route/leaning.js
+++ route/leaning.js
@@ -1,8 +1,19 @@
+
 var leaning = function(req, res){
     console.log("/leaning(get)");
-    
 
+    //run python program
+    var exec = require('child_process').exec;
+    exec('node -v', function(err, stdout, stderr){
+        if(err){
+            cosnole.log('child process error');
+        }
+        else{
+            console.log(stdout);
+        }
+    });
 
+    res.redirect('./public/package.json');
 };
 
 
